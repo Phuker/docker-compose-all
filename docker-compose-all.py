e4a812aee5870582aaaf93bd5c9c491b54ce85bd
Phuker <11596611+Phuker@users.noreply.github.com>
2019-01-03 23:57:42 +0800

    date format add date

--- docker-compose-all.py
+++ docker-compose-all.py
@@ -12,7 +12,7 @@ import argparse
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s [%(levelname)s]:%(message)s',
-    datefmt='%H:%M:%S',
+    datefmt='%Y-%m-%d %H:%M:%S',
     stream=sys.stdout
 )
 
